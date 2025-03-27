2c73d056c24cc475928cd688c748e97a0c34f489
Rahul Maity <96173173+Rahulmaity2003@users.noreply.github.com>
2025-03-27 14:30:06 +0530

    Update BookNotFoundException.java

--- BookNotFoundException.java
+++ BookNotFoundException.java
@@ -1,5 +1,8 @@
 package exception;
 
+/**
+ * Exception for book not found scenarios.
+ */
 public class BookNotFoundException extends Exception {
     public BookNotFoundException(String message) {
         super(message);
