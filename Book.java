464a3e9c12f1dbb8813ed7732528ebc7e4435a9a
Rahul Maity <96173173+Rahulmaity2003@users.noreply.github.com>
2025-03-27 14:24:28 +0530

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,64 @@
+package model;
+
+public class Book {
+    private String bookId;
+    private String title;
+    private String author;
+    private String genre;
+    private String status;
+
+    public Book(String bookId, String title, String author, String genre, String status) {
+        this.bookId = bookId;
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.status = status;
+    }
+
+    // Getters and Setters
+    public String getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(String bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Book ID: " + bookId + ", Title: " + title + ", Author: " + author +
+                ", Genre: " + genre + ", Status: " + status;
+    }
+}
